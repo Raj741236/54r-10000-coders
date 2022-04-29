d8a748c9150055e93274324a9c109c82513ac4a6
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-04-29 10:39:16 +0530

    data types in js

---
+++ Javascript/datatypes.js
@@ -0,0 +1,16 @@
+let firstName = "suresh";
+console.log(typeof firstName);
+
+let age = 26;
+console.log(typeof age);
+
+age = "twenty-six";
+console.log(typeof age);
+
+
+let isJavascriptEasy = true;
+console.log(typeof isJavascriptEasy)
+
+let place=null;
+console.log(typeof place);
+
---
+++ Javascript/introduction_to_js.js
@@ -0,0 +1,23 @@
+       // var river = "Amazon";
+        // var river = "ganga";
+        var river;
+        river = "Amazon";
+        console.log(river)
+
+        // let desert = "sahara";
+        // desert = "thar";
+        let desert;
+        desert = "thar";
+        console.log(desert);
+
+        // const place = "Bangalore";
+        // const place = "Hyderabad";
+        const place="bangalore";
+        console.log(place);
+
+         flavour = "vanilla";
+         flavour = "choclate";
+        console.log(flavour);
+
+        let islandOfIndia = "Andaman";
+        console.log(islandOfIndia);
