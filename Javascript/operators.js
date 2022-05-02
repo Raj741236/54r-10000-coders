9c42bf070d78e1dde027aacff94d58fd83c4e156
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-05-02 08:15:22 +0530

    operators in js

---
+++ Javascript/operators.js
@@ -0,0 +1,35 @@
+//Arithemetic operators
+
+console.log(2 ** 3);//z to the power of 3 /2*2*2
+
+//Assignment Operators
+
+let x = 5;
+console.log(x);
+x += 10; //x = x+10
+console.log(x);
+
+x--;//x = x-1;
+x--;
+console.log(x)
+
+//comparision Operators: output->boolean
+
+//==: it will only check the values but it will ignore datatype
+
+//===: it will check both value and datatype;
+
+console.log(true == "true");
+
+const z = (25 + 25) / 5;
+
+console.log(z)
+
+//Truthy or Falsy Values
+
+console.log(Boolean(0));
+console.log(Boolean('0'));
+console.log(Boolean(undefined));
+console.log(Boolean({}));
+console.log(Boolean('raj'));
+
