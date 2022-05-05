43d5a19ed71b2d33037672d879cd48d4ca41cebd
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-05-05 09:49:44 +0530

    array methods in js

---
+++ Javascript/arraymethods.js
@@ -0,0 +1,61 @@
+//length
+
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// console.log(students);
+// console.log(students.length);
+
+//push method
+
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// students.push("suresh","ramesh");
+// console.log(students);
+
+//pop method
+
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// console.log(students.pop());
+// students.pop();
+// console.log(students);
+
+//unshift method
+
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// students.unshift("suresh","ramesh");
+// console.log(students);
+
+//shift method
+
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// students.shift();
+// console.log(students);
+
+//slice method
+
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// console.log(students.slice(1,2));
+// console.log(students);
+
+//splice method
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// console.log(students.splice());
+// console.log(students);
+
+//includes method
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+
+// console.log(students.includes("chandhan"));
+
+//indexOf method
+// const students = ['chandra','srikanth',"chandra",'meghana','abhishek','hemanth'];
+
+// console.log(students.indexOf("chandra"));
+
+//Reverse method
+// const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+// students.reverse();
+// console.log(students);
+
+
+//join method
+const students = ['chandra','srikanth','meghana','abhishek','hemanth'];
+console.log(students.join('/'));
