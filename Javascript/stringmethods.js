91c6718d93bfe231d2cdb1a82574dfec15958f23
RAJTHEKING69 <88470868+RAJTHEKING69@users.noreply.github.com>
2022-05-06 09:58:41 +0530

    string methods  in js

---
+++ Javascript/stringmethods.js
@@ -0,0 +1,61 @@
+//replace method
+
+// const replaceString = "I am studying JavaScript and JavaScript is an easy language";
+
+// const regex = /javascript/ig;
+
+// const result=replaceString.replace(regex,"React");
+
+// console.log(replaceString);
+
+// console.log(result);
+
+//chatAt Method
+
+// const replaceString = "I am studying JavaScript and JavaScript is an easy language";
+
+// const result = replaceString.charAt(3);
+
+// console.log(result);
+
+//split method
+
+// const replaceString = "I am studying JavaScript and JavaScript is an easy language";
+// const result = replaceString.split();
+// console.log(result);
+
+// const result2 = replaceString.split('')
+// console.log(result2);
+
+// const result3 = replaceString.split(' ');
+// console.log(result3);
+
+//toLowerCase and toUpperCase method
+
+// const replaceString = "I am studying JavaScript and JavaScript is an easy language";
+
+// const result = replaceString.toUpperCase();
+
+// console.log(result);
+
+//trim method
+
+// const replaceString = "      I am studying JavaScript and JavaScript is an easy language      ";             ;
+
+// console.log(replaceString.trim());
+
+//startswith and endswith method
+
+// const replaceString = "I am studying JavaScript and JavaScript is an easy language";
+
+// console.log(replaceString.endsWith("e"));
+
+function greetTheUser(name){
+    console.log("welcome " + name);
+}
+
+greetTheUser("alex");
+greetTheUser('rajesh');
+greetTheUser('ravi');
+
+
